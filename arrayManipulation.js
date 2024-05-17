001c5e75b15fc5ad2db384e273dbb93f3f14b16a
Henry Amponsah <maccurang@outlook.com>
2024-05-17 03:54:28 +0000

    completed task two

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -8,5 +8,22 @@ function processArray(...arr) {
   });
 }
 
-const results = processArray(1, 2, 3, 4, 5);
+const processedArray = processArray(1, 2, 3, 4, 5);
+// console.log(processedArray);
+
+function formatArrayStrings(arrOne, arrTwo) {
+  return arrOne.map((str, index) => {
+    if (arrTwo[index] % 2 == 1) {
+      return str.toLowerCase();
+    } else {
+      return str.toUpperCase();
+    }
+  });
+}
+
+const results = formatArrayStrings(
+  ['Carter', 'Stan', 'Homer', 'Jones', 'Webber'],
+  processedArray
+);
+
 console.log(results);
